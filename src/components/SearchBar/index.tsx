2170526e0b5b38889c6d48cd4b09eacda6bb936a
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-02-28 19:50:08 +0700

    update search bar v1

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import "./searchBar.css";
+import { Link } from "react-router-dom";
 
 const SearchBar: React.FC = () => {
   return (
     <div className="search-box">
       <input type="text" placeholder="Search Bikes, Gear & Accessories" />
-      <FaSearch className="search-icon" />
+      <Link to="/search">
+        <FaSearch className="search-icon" />
+      </Link>
     </div>
   );
 };
