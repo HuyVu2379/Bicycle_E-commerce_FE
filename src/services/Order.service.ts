d6008528dcf5a0a1be5ffd54e2e06acf43088b7e
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-05-18 18:20:35 +0700

    feat: update order fetching logic and enhance order detail display

--- src/services/Order.service.ts
+++ src/services/Order.service.ts
@@ -58,3 +58,51 @@ export const getOrdersByUserId = async (
         };
     }
 };
+
+export const getOrders = async () => {
+    try {
+        const url = `${api}/get-all`;
+        let result = await axiosConfig.get(url);
+        return result;
+    } catch (error) {
+        console.error("Error getting orders:", error);
+        return { success: false, message: error };
+    }
+}
+
+export const getOrdersByPage = async (pageNo: number) => {
+    try {
+        const url = `${api}/get-all?pageNo=${pageNo}`;
+        let result = await axiosConfig.get(url);
+        return result;
+    } catch (error) {
+        console.error("Error getting orders:", error);
+        return { success: false, message: error };
+    }
+}
+
+export const getOrdersByUserId_DB = async (userId: string, pageNo: number) => {
+    try {
+        const url = `${api}/get-by-user/${userId}?pageNo=${pageNo}`;
+        let result = await axiosConfig.get(url);
+        return result;
+    } catch (error) {
+        console.error("Error getting orders:", error);
+        return { success: false, message: error };
+    }
+}
+
+//get order by orderId
+export const getOrderById = async (orderId: string) => {
+    try {   
+        const url = `${api}/get/${orderId}`;
+        const result = await axiosConfig.get(url, {
+            maxRedirects: 0, // chặn redirect và xử lý nó nếu xảy ra
+            validateStatus: (status) => status < 400 // không ném lỗi cho 3xx
+          });
+        return result;
+    } catch (error) {
+        console.error("Error getting order:", error);
+        return { success: false, message: error };
+    }
+}
