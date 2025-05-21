644c893e9a083b634c4ed3cb65c7e069120045f5
HuyVu2379 <21021491.huy@student.iuh.edu.vn>
2025-05-21 21:15:02 +0700

    merge code

--- src/hook/api/useCart.ts
+++ src/hook/api/useCart.ts
@@ -55,21 +55,21 @@ function useCart() {
             console.log("check result bulk delete cart items: ", result);
         }
     }
-    const createCarts = async () => {
-        try {
-            const response = await CartService.createCart();
-            console.log("Check data create Cart: ",response);
+    // const createCarts = async () => {
+    //     try {
+    //         const response = await CartService.createCart();
+    //         console.log("Check data create Cart: ",response);
             
-            if (response.success) {
-                dispatch(setCart(response.data));
-            } else {
-                console.error('Failed to create cart :', response.message);
-            }
-            return response;
-        } catch (error) {
-            console.error('Failed to create cart :', error);
-        }
-    };
-    return { fetchCartByUserId, createCartItem, updateQuantityCartItem, removeCartItem, removeCartItems, countItem, createCarts};
+    //         if (response.success) {
+    //             dispatch(setCart(response.data));
+    //         } else {
+    //             console.error('Failed to create cart :', response.message);
+    //         }
+    //         return response;
+    //     } catch (error) {
+    //         console.error('Failed to create cart :', error);
+    //     }
+    // };
+    return { fetchCartByUserId, createCartItem, updateQuantityCartItem, removeCartItem, removeCartItems, countItem};
 }
 export default useCart;
